fb667d862aa8207a1c1fc453b76891f19a358bf1
Ed <mazacan@gmail.com>
2023-03-09 07:58:34 +0200

    Fixed js-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,25 +5,29 @@ const form = document.querySelector('form');
 form.addEventListener('submit', e => {
   e.preventDefault();
 
-  const formData = new formData(e.currentTarget);
+  const formData = new FormData(e.currentTarget);
   const data = {};
   formData.forEach((value, key) => {
     data[key] = Number(value);
   });
   let delay = data.delay;
   for (let i = 0; i < data.amount; i += 1) {
-    showPromice(createPromise(i + 1, delay));
+    showPromise(createPromise(i + 1, delay));
     delay += data.step;
   }
 });
 
-function showPromice(promise) {
+function showPromise(promise) {
   promise
     .then(({ position, delay }) => {
-      Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`, {
+        useIcon: false,
+      });
     })
     .catch(({ position, delay }) => {
-      Notiflix.Notify.failure(`Reject promise ${position} in ${delay}ms`);
+      Notiflix.Notify.failure(`Reject promise ${position} in ${delay}ms`, {
+        useIcon: false,
+      });
     });
 }
 
@@ -33,8 +37,9 @@ function createPromise(position, delay) {
       const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
         resolve({ position, delay });
+      } else {
+        reject({ position, delay });
       }
-      reject({ position, delay });
     }, delay);
   });
   return promise;
